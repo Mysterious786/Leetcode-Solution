082f3b103daceb5f52e5f8d78f4755af45a61012
Saqlain Zarjis Ansari <110441918+Mysterious786@users.noreply.github.com>
2023-12-25 11:53:23 +0530

    Time: 3 ms (88.54%), Space: 7.5 MB (86.51%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+private:
+    void find(int row, int n, vector<bool>& l, vector<bool>& r, vector<bool>& c, vector<vector<string>>& ans, vector<string>& st) {
+        if (row == n) {
+            ans.push_back(st);
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (!l[row - i + n - 1] && !c[i] && !r[row + i]) {
+                st[row][i] = 'Q';
+                l[row - i + n - 1] = c[i] = r[row + i] = true;
+                find(row + 1, n, l, r, c, ans, st);
+                l[row - i + n - 1] = c[i] = r[row + i] = false;
+                st[row][i] = '.';
+            }
+        }
+    }
+
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> st;
+        for (int i = 0; i < n; i++) {
+            string s = "";
+            for (int j = 0; j < n; j++)
+                s += '.';
+            st.emplace_back(s);
+        }
+
+        vector<vector<string>> ans;
+        vector<bool> left(2 * (n - 1)+5), right(2 * (n - 1)+5);
+        vector<bool> col(n);
+
+        find(0, n, left, right, col, ans, st);
+
+        return ans;
+    }
+};
