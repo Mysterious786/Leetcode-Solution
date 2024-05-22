c94e14b33c991739bb5e2307700d02f2906afa4f
Saqlain Zarjis Ansari <110441918+Mysterious786@users.noreply.github.com>
2024-05-22 17:46:14 +0530

    Time: 16 ms (68.53%), Space: 26.6 MB (91.10%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        //sol1:
-        sort(nums.begin(),nums.end());
-        return nums[nums.size()/2];
+        int count =0,ele=0;
+        for(int i=0;i<nums.size();i++){
+            if(count ==0){
+                ele = nums[i];
+                count = 1;
+            }
+            else if(nums[i]==ele) count++;
+            else count--;
+        }
+        return ele;
         
     }
 };
