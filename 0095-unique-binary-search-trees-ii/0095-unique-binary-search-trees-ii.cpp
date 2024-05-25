00d3ce845900fb80fe419c21a6dd695c1dd54dbd
Saqlain Zarjis Ansari <110441918+Mysterious786@users.noreply.github.com>
2024-05-25 18:49:13 +0530

    Time: 10 ms (67.61%), Space: 19.2 MB (27.71%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
@@ -11,32 +11,30 @@
  */
 class Solution {
     private:
-    vector<TreeNode*> find(int start,int end){
+    vector<TreeNode*> f(int start,int end){
         vector<TreeNode*> ans;
         if(start>end){
             ans.push_back(NULL);
             return ans;
         }
-            for(int i=start;i<=end;i++){
-                vector<TreeNode*> lefty = find(start,i-1);
-                vector<TreeNode*> righty = find(i+1,end);
-                for(int j=0;j<lefty.size();j++){
-                    for(int k=0;k<righty.size();k++){
-                        TreeNode* newNode = new TreeNode(i);
-                        newNode->left = lefty[j];
-                        newNode->right = righty[k];
-                        ans.push_back(newNode);
-                    }
+        for(int i=start;i<=end;i++){
+            vector<TreeNode*> lefty = f(start,i-1);
+            vector<TreeNode*> righty = f(i+1,end);
+            for(auto a : lefty){
+                for(auto b : righty ){
+                    TreeNode* newNode = new TreeNode(i);
+                    newNode->left = a;
+                    newNode->right = b;
+                    ans.push_back(newNode);
                 }
             }
-            
-        
+        }
         return ans;
-        
     }
 public:
     vector<TreeNode*> generateTrees(int n) {
-        return find(1,n);
+        
+        return f(1,n);
         
     }
 };
