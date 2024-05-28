aa03577a05c8c83771d0b0c4c5c4b35d69a13a3b
Saqlain Zarjis Ansari <110441918+Mysterious786@users.noreply.github.com>
2024-05-29 03:09:40 +0530

    Time: 8 ms (46.95%), Space: 8.9 MB (19.77%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
@@ -6,10 +6,9 @@ public:
         int start=0,end=0;
         int n = s.size();
         while(end<n){
-            sum+=abs((s[end]-'0')-(t[end]-'0'));
-            while(sum>maxCost){
-                sum-=abs((s[start]-'0')-(t[start]-'0'));
-                start++;
+           maxCost-=abs((s[end]-'0')-(t[end]-'0'));
+            if(maxCost<0){
+                maxCost+=abs((s[start]-'0')-(t[start++]-'0'));
             }
             ans = max(ans,end-start+1);
             end++;
