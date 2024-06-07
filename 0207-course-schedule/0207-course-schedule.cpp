8806544e9c21250792599edc2165a4c76a18cb51
Saqlain Zarjis Ansari <110441918+Mysterious786@users.noreply.github.com>
2024-06-07 23:37:04 +0530

    Time: 19 ms (36.94%), Space: 17.6 MB (38.70%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& p) {
+        
+        vector<vector<int>> adj(n);
+        for(auto it : p) adj[it[1]].push_back(it[0]);
+        vector<int> ind(n,0);
+        for(int i=0;i<n;i++){
+            for(auto it : adj[i]) ind[it]++;
+        }
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(ind[i]==0) q.push(i);
+        }
+        vector<int> ans;
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            ans.push_back(it);
+            for(auto i : adj[it]){
+                ind[i]--;
+                if(ind[i]==0) q.push(i);
+            }
+            
+        }
+        return ans.size()==n;
+        
+    }
+};
