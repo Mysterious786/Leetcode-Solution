adc3659bc34ba178cc8b333cc3874d9e68194041
Saqlain Zarjis Ansari <110441918+Mysterious786@users.noreply.github.com>
2024-06-08 11:32:25 +0530

    Time: 46 ms (96.31%), Space: 33.9 MB (93.13%) - LeetHub

---
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <queue>
+#include <utility>
+
+using namespace std;
+
+class Solution {
+    const static long long MOD = 1e9 + 7;
+public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+        vector<pair<long long, int>> dis(n, {1e18, 0}); // {shortest distance, number of ways}
+        dis[0] = {0, 1}; // starting node with distance 0 and 1 way to reach
+        
+        vector<vector<pair<int, int>>> adj(n);
+        for (auto& road : roads) {
+            adj[road[0]].push_back({road[1], road[2]});
+            adj[road[1]].push_back({road[0], road[2]}); // because the roads are bidirectional
+        }
+        
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+        pq.push({0, 0});
+        
+        while (!pq.empty()) {
+            auto [edge, node] = pq.top();
+            pq.pop();
+            
+            if (edge > dis[node].first) continue; // Skip if we have already found a shorter way
+            
+            for (auto& [to, W] : adj[node]) {
+                if (dis[to].first > edge + W) {
+                    dis[to].first = edge + W;
+                    dis[to].second = dis[node].second;
+                    pq.push({dis[to].first, to});
+                } else if (dis[to].first == edge + W) {
+                    dis[to].second = (dis[to].second + dis[node].second) % MOD;
+                }
+            }
+        }
+        
+        return dis[n - 1].second;
+    }
+};
