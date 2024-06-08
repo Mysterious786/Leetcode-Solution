3dfe1321aba848c06204b5b38ca7dffe0963eaac
Saqlain Zarjis Ansari <110441918+Mysterious786@users.noreply.github.com>
2024-06-09 02:15:09 +0530

    Time: 191 ms (30.98%), Space: 134.6 MB (87.05%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int, int> modMap; // To store (mod value, first occurrence index)
+        modMap[0] = -1; // To handle the case where the subarray starts from index 0
+        int sum = 0;
+        
+        for (int i = 0; i < n; ++i) {
+            sum += nums[i];
+            int mod = sum % k;
+            if (mod < 0) mod += k; // Ensure the mod is positive
+            
+            if (modMap.find(mod) != modMap.end()) {
+                if (i - modMap[mod] > 1) {
+                    return true; // Found a subarray with at least 2 elements
+                }
+            } else {
+                modMap[mod] = i; // Store the first occurrence of this mod value
+            }
+        }
+        
+        return false;
+    }
+};
+
