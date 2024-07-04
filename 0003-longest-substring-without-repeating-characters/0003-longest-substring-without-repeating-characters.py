82650233a9e17ff55cd88064fd7f78592bfa1c0b
Saqlain Zarjis Ansari <110441918+Mysterious786@users.noreply.github.com>
2024-07-04 08:20:21 +0530

    Time: 65 ms (28.76%), Space: 16.5 MB (78.95%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,22 +4,19 @@ class Solution:
         n = len(s)
         if n == 0:
             return 0
-        start=0
+        start = 0
         end = 0
-        ans = 0
+        ans = 1
         while end < n:
             if s[end] in mp:
-                mp[s[end]] = mp[s[end]] + 1;
+                mp[s[end]] +=1
             else :
                 mp[s[end]] = 1
-            while mp[s[end]]>1:
-                mp[s[start]]-=1;
+            while mp[s[end]] > 1:
+                mp[s[start]] -=1;
                 if mp[s[start]] == 0:
                     del mp[s[start]]
-                
                 start+=1
-            
             ans = max(ans,end-start+1)
             end+=1
         return ans
-            
