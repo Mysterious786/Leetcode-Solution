455c4db7911cee6705fdafe3bcf9cfda40d4fb54
Saqlain Zarjis Ansari <110441918+Mysterious786@users.noreply.github.com>
2024-07-26 07:02:39 +0530

    Time: 4 ms (56.33%), Space: 15 MB (18.89%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        // things needed
-        //1. queue (treeNode,<x,y>)
         queue<pair<TreeNode*,pair<int,int>>> q;
-        // map neede to map things vertical
+        vector<vector<int>> ans;
         map<int,map<int,multiset<int>>> mp;
 
-        // suppose I need to move left and currently on (x,y) that means ===> (x+1,y-1);
-        // For right (x,y) ===> (x+1,y+1);
-
-        // lets push root , root on the origin (0,0)
-
         q.push({root,{0,0}});
+
         while(!q.empty()){
-            auto it = q.front();
+            auto front = q.front();
+            TreeNode* node = front.first;
+            int x = front.second.first;
+            int y = front.second.second;
             q.pop();
-            TreeNode* node = it.first;
-            int x = it.second.first;
-            int y = it.second.second;
             mp[x][y].insert(node->val);
             if(node->left!=NULL){
                 q.push({node->left,{x-1,y+1}});
@@ -28,17 +22,14 @@ public:
             }
         }
 
-        vector<vector<int>> ans;
         for(auto it : mp){
             vector<int> v;
             for(auto i : it.second){
                 v.insert(v.end(),i.second.begin(),i.second.end());
-
             }
             ans.push_back(v);
         }
         return ans;
-
 
         
     }
