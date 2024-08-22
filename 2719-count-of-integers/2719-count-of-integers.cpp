60945a6607f99634e440cefc3d76abfe97c0579e
Saqlain Zarjis Ansari <110441918+Mysterious786@users.noreply.github.com>
2024-08-23 02:45:32 +0530

    Time: 22 ms (51.22%), Space: 8.8 MB (63.76%) - LeetHub

---
+++ 2719-count-of-integers/2719-count-of-integers.cpp
@@ -0,0 +1,47 @@
+class Solution {
+    int dp[25][2][500];
+    const static long long MOD = 1e9+7;
+private:
+    long long f(int ind, bool tight, int sum, int minSum, int maxSum, string& s) {
+        if (sum > maxSum) return 0;
+        if (ind == s.size()) {
+            return (minSum <= sum && sum <= maxSum);
+        }
+        if (dp[ind][tight][sum] != -1) {
+            return dp[ind][tight][sum];
+        }
+        long long count = 0;
+        int limit = tight ? s[ind] - '0' : 9;
+        for (int digit = 0; digit <= limit; digit++) {
+            int newTight = tight && (digit == limit);
+            count = (count + f(ind + 1, newTight, sum + digit, minSum, maxSum, s)) % MOD;
+        }
+        return dp[ind][tight][sum] = count % MOD;
+    }
+public:
+    int count(string num1, string num2, int minSum, int maxSum) {
+        auto reduceByOne = [](string& num) {
+            int n = num.size();
+            for (int i = n - 1; i >= 0; --i) {
+                if (num[i] > '0') {
+                    num[i]--;
+                    break;
+                } else {
+                    num[i] = '9';
+                }
+            }
+            if (num[0] == '0') {
+                num.erase(num.begin());
+            }
+        };
+        
+        memset(dp, -1, sizeof(dp));
+        reduceByOne(num1);
+        long long first = f(0, true, 0, minSum, maxSum, num1);
+        
+        memset(dp, -1, sizeof(dp));
+        long long second = f(0, true, 0, minSum, maxSum, num2);
+        
+        return static_cast<int>((second - first + MOD) % MOD);
+    }
+};
