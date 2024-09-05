fceacd21f735f2ff976f248413d8ce81200e63fe
Saqlain Zarjis Ansari <110441918+Mysterious786@users.noreply.github.com>
2024-09-05 22:30:25 +0530

    Time: 596 ms (48.50%), Space: 182.9 MB (77.54%) - LeetHub

---
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    Node* links[2];  // We use 2 instead of 26 since we only need 0 and 1 for binary numbers
+
+    Node() {
+        links[0] = links[1] = nullptr;  // Initialize pointers to nullptr
+    }
+
+    bool containKey(int bit) {
+        return links[bit] != nullptr;
+    }
+
+    void put(int bit, Node* node) {
+        links[bit] = node;
+    }
+
+    Node* get(int bit) {
+        return links[bit];
+    }
+};
+
+class Trie {
+private:
+    Node* root;
+
+public:
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(int num) {
+        Node* node = root;
+        for (int i = 31; i >= 0; --i) {
+            int bit = (num >> i) & 1;
+            if (!node->containKey(bit)) {
+                node->put(bit, new Node());
+            }
+            node = node->get(bit);
+        }
+    }
+
+    int findMaximum(int num) {
+        Node* node = root;
+        int maxXor = 0;
+        for (int i = 31; i >= 0; --i) {
+            int bit = (num >> i) & 1;
+            int toggledBit = bit ^ 1;  // Try to find the opposite bit for maximizing XOR
+            if (node->containKey(toggledBit)) {
+                maxXor = (maxXor << 1) | 1;  // If found, this bit contributes to the max XOR
+                node = node->get(toggledBit);
+            } else {
+                maxXor = (maxXor << 1);  // Otherwise, keep the current bit
+                node = node->get(bit);
+            }
+        }
+        return maxXor;
+    }
+};
+
+class Solution {
+public:
+    int findMaximumXOR(vector<int>& nums) {
+        Trie trie;
+        int maxXor = 0;
+        for (int num : nums) {
+            trie.insert(num);
+        }
+        for (int num : nums) {
+            maxXor = max(maxXor, trie.findMaximum(num));
+        }
+        return maxXor;
+    }
+};
+
