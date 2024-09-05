d2a31bdad9bcd4c70351d3c67c3ad8690fffaf3f
Saqlain Zarjis Ansari <110441918+Mysterious786@users.noreply.github.com>
2024-09-05 10:32:12 +0530

    Time: 0 ms (100.00%), Space: 10.4 MB (17.20%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -4,15 +4,18 @@ public:
         int n = nums.size();
         int low = 0;
         int high = n-1;
-        int mid  =0;
-        while(mid<=high){
-            if(nums[mid]==0) {swap(nums[low],nums[mid]);low++; mid++;}
-            else if(nums[mid]==2) {
-                swap(nums[high],nums[mid]);
+        int mid = 0;
+        while(mid <= high){
+            if(nums[mid]==1) mid++;
+            else if(nums[mid]==0){
+                swap(nums[mid],nums[low]);
+                low++;
+                mid++;
+            }
+            else{
+                swap(nums[mid],nums[high]);
                 high--;
             }
-            else  mid++;
-           
         }
     }
 };
