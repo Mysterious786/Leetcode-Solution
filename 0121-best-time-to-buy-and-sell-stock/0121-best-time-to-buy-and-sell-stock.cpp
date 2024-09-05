139dc1db06cec9a0579ec1706fec6d4c99ba2774
Saqlain Zarjis Ansari <110441918+Mysterious786@users.noreply.github.com>
2024-09-05 10:47:58 +0530

    Time: 94 ms (20.72%), Space: 96.2 MB (70.18%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -3,11 +3,11 @@ public:
     int maxProfit(vector<int>& prices) {
         int mini = INT_MAX;
         int maxi = INT_MIN;
-        for(auto it : prices){
-            mini = min(it,mini);
-            maxi = max(it-mini,maxi);
+        int n = prices.size();
+        for(int i=0;i<n;i++){
+            mini = min(mini,prices[i]);
+            maxi = max(maxi,prices[i]-mini);
         }
         return maxi;
-        
     }
 };
