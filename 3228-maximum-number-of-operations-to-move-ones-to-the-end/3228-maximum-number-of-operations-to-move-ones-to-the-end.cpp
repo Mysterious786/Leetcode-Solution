9c952cb8c190840cf35a9def932bc11c13edc232
Saqlain Zarjis Ansari <110441918+Mysterious786@users.noreply.github.com>
2024-10-02 09:30:23 +0530

    Time: 29 ms (44.13%), Space: 14.2 MB (68.13%) - LeetHub

--- 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.cpp
+++ 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -2,23 +2,22 @@ class Solution {
 public:
     int maxOperations(string s) {
         int n = size(s);
-        int count  = 0;
+        int count = 0;
         int ans = 0;
-        bool flag = 1;
+        bool flag = true;
         for(int i=0;i<n;i++)
         {
-            char c = s[i];
-            if(c=='1' and flag == 1) count++;
-            if(c=='0' and flag == 1) flag = 0;
-            if(c=='1' and flag==0)
+            char ch = s[i];
+            if(ch == '1' and flag) count++;
+            if(ch == '0' and flag) flag = 0;
+            if(ch == '1' and flag == 0)
             {
                 ans+=count;
                 count++;
                 flag=!flag;
             }
-            
         }
-        if(flag==0) ans+=count;
+        if(!flag) ans+=count;
         return ans;
     }
 };
