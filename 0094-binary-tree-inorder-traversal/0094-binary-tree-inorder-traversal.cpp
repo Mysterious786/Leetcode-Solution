399fecb331644b950e65107b2380011c998d3fda
Saqlain Zarjis Ansari <110441918+Mysterious786@users.noreply.github.com>
2024-10-08 07:05:35 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (71.38%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,17 +11,20 @@
  */
 class Solution {
     private:
-    void helper(TreeNode* root,vector<int>& ans){
-        if(root==NULL) return;
-        helper(root->left,ans);
+    void inorderTraversal(TreeNode* root, vector<int>& ans)
+    {
+        if(root == NULL)
+        {
+            return ;
+        }
+        inorderTraversal(root->left,ans);
         ans.push_back(root->val);
-        helper(root->right,ans);
+        inorderTraversal(root->right,ans);
     }
-                
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-         helper(root,ans);
+        inorderTraversal(root,ans);
         return ans;
     }
 };
