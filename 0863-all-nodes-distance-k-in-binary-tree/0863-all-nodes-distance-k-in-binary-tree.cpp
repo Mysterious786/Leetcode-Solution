2a3caec4606ae752b3b4d358f250551536df4425
Saqlain Zarjis Ansari <110441918+Mysterious786@users.noreply.github.com>
2024-10-08 11:42:52 +0530

    Time: 6 ms (41.26%), Space: 15.9 MB (16.79%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -1,45 +1,62 @@
-void makeParent(TreeNode* root,unordered_map<TreeNode*,TreeNode*> &parent){
-    queue<TreeNode*> q;
-    q.push(root);
-    while(!q.empty()){
-        int n= q.size();
-        for(int i=0;i<n;i++){
-           TreeNode* node= q.front();
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    private:
+    void makeParent(TreeNode* root, map<TreeNode*,TreeNode*>& parent)
+    {
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            auto front = q.front();
             q.pop();
-            if(node->left) {
-                parent[node->left]=node;
-                q.push(node->left);
+            if(front->left)
+            {
+                parent[front->left] = front;
+                q.push(front->left);
             }
-            if(node->right){
-                parent[node->right]=node;
-                q.push(node->right);
+            if(front->right)
+            {
+                parent[front->right] = front;
+                q.push(front->right);
             }
         }
     }
-}
-class Solution {
-public:
-    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        unordered_map<TreeNode*,TreeNode*> parent;
-        makeParent(root,parent);
-        unordered_map<TreeNode*,bool> visited;
-        vector<int> ans;
-        solve(target,parent,visited,k,ans);
-        return ans;
-    }
-    void solve(TreeNode* target,unordered_map<TreeNode*,TreeNode*> &parent,unordered_map<TreeNode*,bool> &visited,int k,vector<int> &ans){
-        if(k==0){
+    void solve(TreeNode* target,map<TreeNode*,bool>& visited,map<TreeNode*,TreeNode*>& parent,vector<int>& ans,int k)
+    {
+        if(k==0)
+        {
             ans.push_back(target->val);
         }
-        visited[target]=true;
-        if(target->left && !visited[target->left]){
-            solve(target->left,parent,visited,k-1,ans);
+        visited[target] = true;
+        if(!visited[target->left] and target->left!=NULL)
+        {
+            solve(target->left,visited,parent,ans,k-1);
         }
-        if(target->right && !visited[target->right]){
-            solve(target->right,parent,visited,k-1,ans);
+        if(!visited[target->right] and target->right!=NULL)
+        {
+            solve(target->right,visited,parent,ans,k-1);
         }
-        if(parent[target]!=NULL && !visited[parent[target]]){
-            solve(parent[target],parent,visited,k-1,ans);
+        if(!visited[parent[target]] and parent[target]!=NULL)
+        {
+            solve(parent[target],visited,parent,ans,k-1);
         }
+        
+    }
+public:
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        vector<int> ans;
+        map<TreeNode*,TreeNode*> parent;
+        map<TreeNode*,bool> visited;
+        makeParent(root,parent);
+        solve(target,visited,parent,ans,k);
+        return ans;
     }
 };
