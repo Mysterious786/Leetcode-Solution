8b6db002821b538f6c4e077168468062ddc323a0
Saqlain Zarjis Ansari <110441918+Mysterious786@users.noreply.github.com>
2024-10-08 08:53:22 +0530

    Time: 5 ms (30.67%), Space: 14.9 MB (50.50%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,36 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        queue<pair<TreeNode*,pair<int,int>>> q;
-        vector<vector<int>> ans;
         map<int,map<int,multiset<int>>> mp;
-
+        queue<pair<TreeNode*,pair<int,int>>> q;
         q.push({root,{0,0}});
-
-        while(!q.empty()){
-            auto front = q.front();
-            TreeNode* node = front.first;
-            int x = front.second.first;
-            int y = front.second.second;
+        while(!q.empty())
+        {
+            auto node = q.front();
             q.pop();
-            mp[x][y].insert(node->val);
-            if(node->left!=NULL){
-                q.push({node->left,{x-1,y+1}});
-            }
-            if(node->right!=NULL){
-                q.push({node->right,{x+1,y+1}});
-            }
+            int col = node.second.first;
+            int row = node.second.second;
+            auto nd = node.first;
+            mp[col][row].insert(nd->val);
+            if(nd->left) q.push({nd->left,{col-1,row+1}});
+            if(nd->right)q.push({nd->right,{col+1,row+1}});
+            
         }
-
-        for(auto it : mp){
+        
+        vector<vector<int>> ans;
+       for(auto it : mp)
+        {
             vector<int> v;
-            for(auto i : it.second){
+            for(auto i : it.second)
+            {
                 v.insert(v.end(),i.second.begin(),i.second.end());
             }
             ans.push_back(v);
         }
         return ans;
-
+        
         
     }
 };
