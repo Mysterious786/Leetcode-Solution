e62d4db2d9d11b2a08232d5a53466a3ac6f52ea5
Saqlain Zarjis Ansari <110441918+Mysterious786@users.noreply.github.com>
2024-11-11 12:38:01 +0530

    Time: 3 ms (44.05%), Space: 15 MB (34.01%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,35 +1,23 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
+        vector<vector<int>> ans;
         map<int,map<int,multiset<int>>> mp;
-        queue<pair<TreeNode*,pair<int,int>>> q;
-        q.push({root,{0,0}});
+        queue<pair<pair<int,int>,TreeNode*>> q;
+        q.push({{0,0},root});
+
         while(!q.empty())
         {
-            auto node = q.front();
+            auto front = q.front();
             q.pop();
-            int col = node.second.first;
-            int row = node.second.second;
-            auto nd = node.first;
-            mp[col][row].insert(nd->val);
-            if(nd->left) q.push({nd->left,{col-1,row+1}});
-            if(nd->right)q.push({nd->right,{col+1,row+1}});
-            
+            int col = front.first.first;
+            int row = front.first.second;
+            TreeNode* node = front.second;
+            mp[col][row].insert(node->val);
+            if(node->left) q.push({{col-1,row+1},node->left});
+            if(node->right) q.push({{col+1,row+1},node->right});
         }
-        
-        vector<vector<int>> ans;
-       for(auto it : mp)
+        for(auto it : mp)
         {
             vector<int> v;
             for(auto i : it.second)
@@ -39,7 +27,5 @@ public:
             ans.push_back(v);
         }
         return ans;
-        
-        
     }
 };
