25228163e9217dd5ccdcc04a2c72f1fdf12eaa1c
Saqlain Zarjis Ansari <110441918+Mysterious786@users.noreply.github.com>
2024-11-12 02:22:13 +0530

    Time: 2 ms (100.00%), Space: 7.7 MB (9.61%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,14 @@
 class Solution {
-    private:
-    int f(int stair,int n,vector<int>& dp){
-        if(stair > n) return 0;
-        if(stair == n) return 1;
-        if(dp[stair]!=-1) return dp[stair];
-        int first = f(stair+1,n,dp);
-        int second = f(stair+2,n,dp);
-        return dp[stair] = first+second;
-    }
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return f(0,n,dp);
-        
+        if(n==1) return 1;
+        vector<int> dp(n+1,0);
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3;i<=n;i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
