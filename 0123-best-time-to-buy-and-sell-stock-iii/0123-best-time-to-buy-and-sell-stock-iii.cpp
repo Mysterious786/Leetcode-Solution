b4ba94ff43dede657e4c0021d240b734152b8855
Saqlain Zarjis Ansari <110441918+Mysterious786@users.noreply.github.com>
2024-11-15 10:23:24 +0530

    Time: 42 ms (70.54%), Space: 88.6 MB (64.89%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+    int dp[100003][2][3];
+private:
+    int totalMaxProfit(int ind, int byOrSell,vector<int>& prices, int n,int transactions)
+    {
+        if(transactions == 0) return 0;
+        // base case
+        if(ind == n)
+        {
+            return 0;
+        }
+        
+        if(dp[ind][byOrSell][transactions]!=-1) return dp[ind][byOrSell][transactions];
+        int buy = 0,sell = 0;
+
+        if(!byOrSell) // no stock currently
+        {
+            buy = max(totalMaxProfit(ind+1,byOrSell,prices,n,transactions),-prices[ind] + totalMaxProfit(ind+1,!byOrSell,prices,n,transactions));
+        }
+        else
+        {
+            sell = max(totalMaxProfit(ind+1,byOrSell,prices,n,transactions),prices[ind] + totalMaxProfit(ind+1,!byOrSell,prices,n,transactions-1));
+        }
+        return dp[ind][byOrSell][transactions] = max(buy,sell);
+    }
+public:
+    int maxProfit(vector<int>& prices) {
+     int n = prices.size();
+     // atmost2 transaction
+     memset(dp,-1,sizeof(dp));
+     // dp[ind][buy/sell].... buy == false sale = false
+     // when a buy + sell occur then its a transactions OOOOK..
+
+     return totalMaxProfit(0,0,prices,n,2);   
+    }
+};
