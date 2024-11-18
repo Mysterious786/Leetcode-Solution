e2e362b2e3a1c140ad10a0f17796f1477bdf7e96
Saqlain Zarjis Ansari <110441918+Mysterious786@users.noreply.github.com>
2024-11-18 22:59:42 +0530

    Time: 0 ms (100.00%), Space: 15.3 MB (10.08%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,32 +1,37 @@
 class Solution {
-    map<int,int> mp;
-    private:
-    void dfs(int node,vector<int>& vis,vector<vector<int>>& adj){
+    vector<vector<int>> adj;
+    vector<int> vis;
+
+    void dfs(int node) {
         vis[node] = 1;
-        if(mp.find(node)==mp.end()) return;
-        for(auto it : adj[node]){
-            if(vis[it]==0){
-                mp[it]++;
-                dfs(it,vis,adj);
+        for (auto it : adj[node]) {
+            if (!vis[it]) {
+                dfs(it);
             }
         }
     }
+
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        vector<vector<int>> adj(rooms.size());
-        for(int i=0;i<rooms.size();i++){
-            for(int j=0;j<rooms[i].size();j++){
+        int n = rooms.size();
+        vis.clear();
+        adj.clear();
+        vis.resize(n, 0);
+        adj.resize(n);
+        
+        // Construct adjacency list
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < rooms[i].size(); j++) {
                 adj[i].push_back(rooms[i][j]);
             }
         }
-        vector<int> vis(rooms.size());
-        mp[0]++;
-        int n = rooms.size();
-        for(int i=0;i<n;i++){
-            if(vis[i]==0) dfs(i,vis,adj);
-        }
-        for(int i=0;i<rooms.size();i++){
-            if(mp.find(i)==mp.end()) return false;
+
+        // Perform DFS from room 0
+        dfs(0);
+
+        // Check if all rooms are visited
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) return false;
         }
         return true;
     }
