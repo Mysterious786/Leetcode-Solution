c45ccf2a51a53691ff81b2042d73aa797517eea2
Saqlain Zarjis Ansari <110441918+Mysterious786@users.noreply.github.com>
2024-11-19 08:20:12 +0530

    Time: 4 ms (35.23%), Space: 20 MB (21.40%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,38 +1,44 @@
 class Solution {
-private:
-    void dfs(int node, vector<vector<int>>& adj, vector<int>& vis) {
+    vector<int> vis;
+    vector<vector<int>> adj;
+    void dfs(int node)
+    {
         vis[node] = 1;
-        for (auto it : adj[node]) {
-            if (!vis[it]) {
-                dfs(it, adj, vis);
+        for(auto it : adj[node])
+        {
+            if(!vis[it])
+            {
+                dfs(it);
             }
         }
     }
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size(); // number of nodes
-        vector<int> vis(n, 0); // visited array of size n
-        int count = 0;
-        vector<vector<int>> adj(n); // adjacency list of size n
-
-        // Constructing adjacency list
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (isConnected[i][j] == 1 && i != j) {
+        vis.clear();
+        adj.clear();
+        int n = isConnected.size();
+        vis.resize(n,0);
+        adj.resize(n);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<isConnected[i].size();j++)
+            {
+                if(isConnected[i][j])
+                {
                     adj[i].push_back(j);
                     adj[j].push_back(i);
                 }
             }
         }
-
-        // Perform DFS for each component
-        for (int i = 0; i < n; i++) {
-            if (!vis[i]) {
-                count++;
-                dfs(i, adj, vis);
+        int province = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i])
+            {
+                dfs(i);
+                province++;
             }
         }
-
-        return count;
+        return province;
     }
 };
