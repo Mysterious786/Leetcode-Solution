7973fea7b6f744126d29ebb458b814b4b42a862d
Saqlain Zarjis Ansari <110441918+Mysterious786@users.noreply.github.com>
2024-11-23 11:21:54 +0530

    Time: 9 ms (22.92%), Space: 18 MB (35.55%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,28 +1,44 @@
 class Solution {
 public:
-    vector<int> findOrder(int n, vector<vector<int>>& p) {
-          vector<vector<int>> adj(n);
-        for(auto it : p) adj[it[1]].push_back(it[0]);
-        vector<int> ind(n,0);
-        for(int i=0;i<n;i++){
-            for(auto it : adj[i]) ind[it]++;
+    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(n);
+        vector<int> indegree(n,0);
+        for(auto it : prerequisites)
+        {
+            int u = it[0];
+            int v = it[1];
+            indegree[u]++;
+            adj[v].push_back(u);
         }
         queue<int> q;
-        for(int i=0;i<n;i++){
-            if(ind[i]==0) q.push(i);
-        }
-        vector<int> ans;
-        while(!q.empty()){
-            auto it = q.front();
-            q.pop();
-            ans.push_back(it);
-            for(auto i : adj[it]){
-                ind[i]--;
-                if(ind[i]==0) q.push(i);
+        vector<int> topo;
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i] == 0)
+            {
+                q.push(i);
+
             }
-            
         }
-        if(ans.size()!=n) return {};
-        return ans;
+        while(!q.empty())
+        {
+            int node =q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it : adj[node])
+            {
+                indegree[it]--;
+                if(indegree[it] == 0)
+                {
+                    q.push(it);
+                }
+            }
+        }
+        if(topo.size()>n or topo.size()<n)
+        {
+            return {};
+        }
+        return topo;
+
     }
 };
